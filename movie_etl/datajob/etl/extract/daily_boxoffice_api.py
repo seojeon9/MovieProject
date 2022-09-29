a90b4b1f65adfa5d499589b6659077f13e97bb4a
seojeon9 <underloose18@gmail.com>
2022-09-29 17:25:59 +0900

    Update daily_boxoffice_api

--- movie_etl/datajob/etl/extract/daily_boxoffice_api.py
+++ movie_etl/datajob/etl/extract/daily_boxoffice_api.py
@@ -1,14 +1,10 @@
 from multiprocessing import get_logger
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 import json
-import pandas as pd
-
 from infra.hdfs_client import get_client
 from infra.util import cal_std_day_yyyymmdd, execute_rest_api
 
 
-class DailyBoxOfficeExtractor:
+class DailyBoxofficeExtractor:
     URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
     SERVICE_KEY = '3ad2d800ada4ee67f4d049b66e903680'
     FILE_DIR = '/movie/daily_box_office/'
@@ -62,34 +58,3 @@ class DailyBoxOfficeExtractor:
         date_list = str(date).split()[0].split('-')
         date_str = date_list[0]+date_list[1]+date_list[2]
         return date_str
-
-
-''' 상엽님 코드
-        date = datetime(2018, 2, 1)
-        date_str = cls.__date_string(date)
-
-        print(date_str)
-
-        while date_str != '20180101':
-            date = date - timedelta(days=1)
-            date_str = cls.__date_string(date)
-
-            url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=3ad2d800ada4ee67f4d049b66e903680&targetDt=' + date_str
-            res = requests.get(url)
-            print(res)
-    
-            text = res.text
-
-            d = json.loads(text)
-            print(d)
-            movie_list = []
-
-            x = d['boxOfficeResult']['showRange']
-            print(x)
-            for b in d['boxOfficeResult']['dailyBoxOfficeList']:
-                movie_list.append([x, b['rnum'], b['rank'], b['rankInten'], b['rankOldAndNew'], b['movieCd'], b['movieNm'], b['openDt'], b['salesAmt'],
-                                   b['salesShare'], b['salesInten'], b['salesChange'], b['salesAcc'], b['audiCnt'], b['audiInten'], b['audiChange'], b['audiAcc'], b['scrnCnt'], b['showCnt']])
-            data = pd.DataFrame(movie_list)
-            data.to_csv("movie_list.csv", mode='a',
-                        encoding='cp949', index=False)
-'''
