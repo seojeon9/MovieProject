05bb1bfb48d61eaadfd29207943c3d840c4fd7aa
seojeon9 <underloose18@gmail.com>
2022-10-03 01:54:55 +0900

    [네이버 검색 ETL] 테스트 모듈 추가

--- movie_etl/tests/naver_test.py
+++ movie_etl/tests/naver_test.py
@@ -5,13 +5,13 @@ from datajob.etl.transform.movie_url_and_actors_transform import MovieUrlAndActo
 
 
 class MTest(unittest.TestCase):
-    # def test1(self):
-    #     NaverSearchMovieExtractor.extract_data()
-
     def test1(self):
-        NaverDatalabApiExtractor.extract_data()
+        NaverSearchMovieExtractor.extract_data()
 
     def test2(self):
+        NaverDatalabApiExtractor.extract_data()
+
+    def test3(self):
         MovieUrlAndActorsTransformer.transform()
 
 
