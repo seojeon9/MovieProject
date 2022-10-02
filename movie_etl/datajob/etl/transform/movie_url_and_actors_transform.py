ed9076e77b40156516149583c8ea746f047e6df3
seojeon9 <underloose18@gmail.com>
2022-10-03 01:51:14 +0900

    udpate movie_url_transform

--- movie_etl/datajob/etl/transform/movie_url_and_actors_transform.py
+++ movie_etl/datajob/etl/transform/movie_url_and_actors_transform.py
@@ -2,33 +2,45 @@ from pyspark.sql.functions import col
 import pandas as pd
 from infra.jdbc import DataWarehouse, save_data
 from infra.spark_session import get_spark_session
+from infra.util import get_movie_codes
 
 
 class MovieUrlAndActorsTransformer:
 
     @classmethod
     def transform(cls):
-        path = '/naver/search_movie/naver_search_movie_20211472.json'
-        movie = get_spark_session().read.option(
-            "multiline", "true").json(path, encoding='UTF-8').first()
+        movie_codes, movie_names = get_movie_codes()
 
-        items = get_spark_session().createDataFrame(movie['items'])
-        items = items['actor', 'link', 'title'].first()
+        for movie_code in movie_codes:
+            path = '/naver/search_movie/naver_search_movie_' + movie_code + '.json'
+            movie = get_spark_session().read.option(
+                "multiline", "true").json(path, encoding='UTF-8').first()
 
-        actors = items.actor
-        actors = actors.split('|')
-        actors = actors[:-1]
-        link = items.link
+            items = get_spark_session().createDataFrame(movie['items'])
+            items = items['actor', 'link', 'title'].first()
+            print(items)
 
-        movie_codes = []
-        for ac in actors:
-            movie_codes.append(('20211472'))
+            actors = items.actor
+            actors = actors.split('|')
+            actors = actors[:-1]
+            link = items.link
 
-        actors_df = pd.DataFrame({
-            'MOVIE_CODE': movie_codes,
-            'ACTOR_NAME': actors
-        })
-        actors_df = get_spark_session().createDataFrame(actors_df)
+            movie_codes = []
+            for ac in actors:
+                movie_codes.append(movie_code)
 
-        print(actors_df)
-        save_data(DataWarehouse, actors_df, 'ACTORS')
+            actors_df = pd.DataFrame({
+                'MOVIE_CODE': movie_codes,
+                'ACTOR_NAME': actors
+            })
+            actors_df = get_spark_session().createDataFrame(actors_df)
+
+            link_df = pd.DataFrame({
+                'MOVIE_CODE': [movie_code],
+                'URL': [link]
+            })
+            link_df = get_spark_session().createDataFrame(link_df)
+
+            # print(actors_df)
+            save_data(DataWarehouse, actors_df, 'ACTORS')
+            save_data(DataWarehouse, link_df, 'MOVIE_URL')
