e265af1a78b4f6da38d4001cf5e63716645d74de
seojeon9 <underloose18@gmail.com>
2022-10-03 02:21:43 +0900

    [네이버 검색 ETL] tramsform 함수 캡슐화

--- movie_etl/datajob/etl/transform/movie_url_and_actors_transform.py
+++ movie_etl/datajob/etl/transform/movie_url_and_actors_transform.py
@@ -12,25 +12,25 @@ class MovieUrlAndActorsTransformer:
         movie_codes, movie_names = get_movie_codes()
 
         for movie_code in movie_codes:
-            movie = cls.load_json(movie_code)
-            items = cls.create_dataframe(movie)
-            cls.save_actors(movie_code, items)
-            cls.save_link(movie_code, items)
+            movie = cls.__load_json(movie_code)
+            items = cls.__create_dataframe(movie)
+            cls.__save_actors(movie_code, items)
+            cls.__save_link(movie_code, items)
 
     @classmethod
-    def save_link(cls, movie_code, items):
-        link_df = cls.generate_link_df(movie_code, items)
+    def __save_link(cls, movie_code, items):
+        link_df = cls.__generate_link_df(movie_code, items)
         link_df = get_spark_session().createDataFrame(link_df)
         save_data(DataWarehouse, link_df, 'MOVIE_URL')
 
     @classmethod
-    def save_actors(cls, movie_code, items):
-        actors_df = cls.generate_actors_df(movie_code, items)
+    def __save_actors(cls, movie_code, items):
+        actors_df = cls.__generate_actors_df(movie_code, items)
         actors_df = get_spark_session().createDataFrame(actors_df)
         save_data(DataWarehouse, actors_df, 'ACTORS')
 
     @classmethod
-    def generate_link_df(cls, movie_code, items):
+    def __generate_link_df(cls, movie_code, items):
         link = items.link
         link_df = pd.DataFrame({
             'MOVIE_CODE': [movie_code],
@@ -40,7 +40,7 @@ class MovieUrlAndActorsTransformer:
         return link_df
 
     @classmethod
-    def generate_actors_df(cls, movie_code, items):
+    def __generate_actors_df(cls, movie_code, items):
         actors = items.actor
         actors = actors.split('|')
         actors = actors[:-1]
@@ -57,14 +57,14 @@ class MovieUrlAndActorsTransformer:
         return actors_df
 
     @classmethod
-    def create_dataframe(cls, movie):
+    def __create_dataframe(cls, movie):
         items = get_spark_session().createDataFrame(movie['items'])
         items = items['actor', 'link', 'title'].first()
         print(items)
         return items
 
     @classmethod
-    def load_json(cls, movie_code):
+    def __load_json(cls, movie_code):
         path = '/naver/search_movie/naver_search_movie_' + movie_code + '.json'
         movie = get_spark_session().read.option(
             "multiline", "true").json(path, encoding='UTF-8').first()
