4730a7c17235231c0e3afbbd3d2255c2c674750f
seojeon9 <underloose18@gmail.com>
2022-10-03 22:14:51 +0900

    [네이버 검색 api] extract 리팩터링

--- movie_etl/datajob/etl/extract/naver_search_api.py
+++ movie_etl/datajob/etl/extract/naver_search_api.py
@@ -4,6 +4,7 @@ from infra.hdfs_client import get_client
 import urllib.request
 
 from infra.jdbc import DataWarehouse, find_data
+from infra.util import get_movie_codes
 
 
 class NaverSearchMovieExtractor:
@@ -19,45 +20,52 @@ class NaverSearchMovieExtractor:
     @classmethod
     def extract_data(cls):
 
-        data = find_data(DataWarehouse, 'MOVIE_DETAIL')
-        # print(type(data))  # pyspark.sql.dataframe.DataFrame
-        data = data.select('MOVIE_CODE', 'MOVIE_NAME')
-        data = data.to_pandas_on_spark()
-        for dt in data.values:
-            print(dt)
-            cls.movie_codes.append(dt[0])
-            cls.movie_names.append(dt[1])
+        cls.movie_codes, cls.movie_names = get_movie_codes()
 
         for i in range(len(cls.movie_codes)):
             code = cls.movie_codes[i]
             name = cls.movie_names[i]
 
-            params = {
-                'query': name
-            }
+            params = cls.__create_param(name)
 
-            encText = urllib.parse.quote(name)
-            url = cls.URL + encText
-            request = urllib.request.Request(url)
-            request.add_header("X-Naver-Client-Id", cls.CLIENT_ID)
-            request.add_header("X-Naver-Client-Secret", cls.CLIENT_KEY)
-            response = urllib.request.urlopen(request)
+            response = cls.__get_never_search_api(name)
             rescode = response.getcode()
 
-            try:
-                if (rescode == 200):
-                    response_body = response.read()
-                    res = response_body.decode('utf-8')
-                    print(res)
-                    file_name = 'naver_search_movie_' + code + '.json'
-                    cls.__upload_to_hdfs(file_name, res)
-                else:
-                    print("Error Code:" + rescode)
-            except Exception as e:
-                print('예외발생 : ', e)
-                log_dict = cls.__create_log_dict(params)
-                cls.__dump_log(log_dict, e)
-                raise e
+            cls.__upload_to_hdfs_from_response(code, params, response, rescode)
+
+    @classmethod
+    def __upload_to_hdfs_from_response(cls, code, params, response, rescode):
+        try:
+            if (rescode == 200):
+                response_body = response.read()
+                res = response_body.decode('utf-8')
+                file_name = 'naver_search_movie_' + code + '.json'
+                cls.__upload_to_hdfs(file_name, res)
+            else:
+                print("Error Code:" + rescode)
+        except Exception as e:
+            print('예외발생 : ', e)
+            log_dict = cls.__create_log_dict(params)
+            cls.__dump_log(log_dict, e)
+            raise e
+
+    @classmethod
+    def __get_never_search_api(cls, name):
+        encText = urllib.parse.quote(name)
+        url = cls.URL + encText
+        request = urllib.request.Request(url)
+        request.add_header("X-Naver-Client-Id", cls.CLIENT_ID)
+        request.add_header("X-Naver-Client-Secret", cls.CLIENT_KEY)
+        response = urllib.request.urlopen(request)
+        return response
+
+    @classmethod
+    def __create_param(cls, name):
+        params = {
+            'query': name
+        }
+
+        return params
 
     @classmethod
     def __upload_to_hdfs(cls, file_name, res):
