e836b1121ef051132fb5d07bf6ed0c80ba387aa3
Jisu Son <s.zisu0417@gmail.com>
2022-10-04 22:48:33 +0900

    Update test code

--- movie_etl/tests/movie_score_test.py
+++ movie_etl/tests/movie_score_test.py
@@ -1,25 +1,12 @@
 import unittest
-from datajob.datamart.actor_datamart import Actor
-from datajob.datamart.movie_score_datamart import MovieScore
-from datajob.datamart.movie_search_datamart import MovieSearch
 from datajob.etl.extract.movie_score import MovieScoreExtractor
 from datajob.etl.transform.movie_score_transform import MovieScoreTransformer
 
 class MTest(unittest.TestCase):
     def test1(self):
         MovieScoreExtractor.extract_data()
-    
     def test2(self):
         MovieScoreTransformer.transform()
-
-    def test3(self):
-        MovieScore.save()
-
-    def test4(self):
-        Actor.save()
-    
-    def test5(self):
-        MovieSearch.save()
 
 
 if __name__ == "__main__":
