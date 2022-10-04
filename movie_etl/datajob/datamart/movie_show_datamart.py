5f95c9fa8ab5c7823c6fe399e925a727fb6a7918
seojeon9 <underloose18@gmail.com>
2022-10-05 02:36:10 +0900

    [Data Mart] movie_show 리팩토링

--- movie_etl/datajob/datamart/movie_show_datamart.py
+++ movie_etl/datajob/datamart/movie_show_datamart.py
@@ -11,6 +11,36 @@ class MovieShow:
         movie_detail = find_data(DataWarehouse, 'MOVIE_DETAIL')
         movie_hit = find_data(DataMart, 'MOVIE_HIT')
 
+        select_open_show_df = cls.__select_open_show_df(
+            movie_box_office, movie_detail)
+
+        open_show_avg_df = cls.__groupby_avg_show_df(
+            movie_box_office, select_open_show_df)
+
+        movie_show_df = cls.__join_hit_grade(movie_hit, open_show_avg_df)
+
+        save_data(DataMart, movie_show_df, 'MOVIE_SHOW')
+
+    @classmethod
+    def __join_hit_grade(cls, movie_hit, open_show_avg_df):
+        hit_select_df = movie_hit.select('MOVIE_CODE', 'HIT_GRADE')
+
+        movie_show_df = open_show_avg_df.join(
+            hit_select_df, on='MOVIE_CODE', how='left')
+
+        return movie_show_df
+
+    @classmethod
+    def __groupby_avg_show_df(cls, movie_box_office, select_open_show_df):
+        avg_show_df = movie_box_office.groupby(
+            movie_box_office.MOVIE_CODE).agg(avg('SHOW_CNT').alias('AVG_SHOW_CNT'))
+        open_show_avg_df = select_open_show_df.join(
+            avg_show_df, on='MOVIE_CODE', how='left')
+
+        return open_show_avg_df
+
+    @classmethod
+    def __select_open_show_df(cls, movie_box_office, movie_detail):
         movie_detail_open_show_df = movie_detail.join(
             movie_box_office, on=['MOVIE_CODE', 'MOVIE_NAME'], how='left')
         # movie_detail_open_show = movie_detail_open_show.select('MOVIE_CODE','MOVIE_NAME','SHOW_CNT')\
@@ -18,16 +48,4 @@ class MovieShow:
         select_open_show_df = movie_detail_open_show_df.select('MOVIE_CODE', 'MOVIE_NAME', movie_detail_open_show_df.SHOW_CNT.alias('OPEN_SHOW_CNT'))\
             .where(movie_detail_open_show_df.STD_DATE == '20220801')
 
-        avg_show_df = movie_box_office.groupby(
-            movie_box_office.MOVIE_CODE).agg(avg('SHOW_CNT').alias('AVG_SHOW_CNT'))
-        open_show_avg_df = select_open_show_df.join(
-            avg_show_df, on='MOVIE_CODE', how='left')
-
-        hit_select_df = movie_hit.select('MOVIE_CODE', 'HIT_GRADE')
-
-        movie_show_df = open_show_avg_df.join(
-            hit_select_df, on='MOVIE_CODE', how='left')
-
-        movie_show_df.show()
-
-        save_data(DataMart, movie_show_df, 'MOVIE_SHOW')
+        return select_open_show_df
