6fc7aeb5b6217fb68c7773820024a36a34882190
Beigee <dbtmdwhd17@gmail.com>
2022-10-06 13:13:27 +0900

    [rest api urls]

--- movie_rest/app/urls.py
+++ movie_rest/app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-router = routers.DefaultRouter()
-# router.register(r'movie/movie_audi', rest_view.MovieAudiViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'movie/movie_actor', rest_view.MovieAudiViewSet)
+
+
 
 
 urlpatterns = [
